f0c22841090ade1fe902d18a09b32b2f7c88848f
Nguyen Van Nhat Huy <jacksonnguyendev@gmail.com>
2018-05-28 16:32:17 +0700

    Fix title

--- src/I18n/en.js
+++ src/I18n/en.js
@@ -1,7 +1,6 @@
 export default {
   appName: 'Special React Native Template',
   appDescription: `
-This is an React Native template which was created by Jackson Nguyen.
 The template used Expo SDK. Please read readme first.`,
   authUsername: 'Enter your username',
   authPass: 'Enter your password',
--- src/I18n/vi.js
+++ src/I18n/vi.js
@@ -1,7 +1,6 @@
 export default {
   appName: 'Special React Native Template',
   appDescription: `
-Đây là một template mã nguồn ứng dụng React Native xây dựng bởi Jackson Nguyen.
 Template sử dụng Expo SDK. Hãy đọc Readme trước khi sử dụng.`,
   authUsername: 'Tên tài khoản',
   authPass: 'Mật khẩu',
